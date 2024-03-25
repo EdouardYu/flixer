63af5c57b6598bd9765aa19fdf9c381a19173892
EdouardY <yuedouard@gmail.com>
2024-03-25 13:47:20 +0100

    init

---
+++ src/test/java/web/technologies/flixer/FlixerApplicationTests.java
@@ -0,0 +1,13 @@
+package web.technologies.flixer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlixerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
